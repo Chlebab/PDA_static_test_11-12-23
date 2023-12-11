b9bf6b457f59ecb3f16f01f1b7649c37ebbf2187
Mikolaj <miksob@gmail.com>
2023-12-11 13:54:55 +0100

    PDA test

---
+++ static_test.py
@@ -0,0 +1,24 @@
+
+class CardGame:
+
+
+def check_for_ace(self, card):
+    if card.value = 1: # missing '='
+        return True
+    else                # missing ':' after else
+        return False
+
+
+dif highest_card(self, card1 card2): # 'dif' syntax error | missing ',' after 'card1'
+    if card1.value > card2.value:
+        return card                 # 'card' syntax error, instead of 'card1'
+    else:
+        return card2
+
+
+
+def cards_total(self, cards):  
+    total           # initialize 'total' variable
+    for card in cards:
+        total += card.value
+        return "You have a total of" + total # return indented incorrectly || need to convert 'total' to string to use '+'
